29d83ee68eaea1545d8ef756b2fce121fbe2caf8
lilkost <111429255+lilkost@users.noreply.github.com>
2024-09-12 10:40:01 +0300

    Add files via upload

--- local/templates/main/assets/js/components/slider.js
+++ local/templates/main/assets/js/components/slider.js
@@ -1,10 +1,26 @@
 const slider = ()=> {
-    const slider = document.querySelector('.result-slider');
-    const swiper = new Swiper(slider, {
+    // const slider = document.querySelector('.result-slider');
+    const swiper = new Swiper('.result-slider', {
         direction: 'horizontal',
         loop: false,
-        slidesPerView: 5.726,
+        slidesPerView:"auto",
         spaceBetween: 16,
+        // breakpoints: {
+        //     993: {
+        //         slidesPerView: "auto",
+        //         spaceBetween: 16,
+        //     },
+
+        //     769: {
+        //         slidesPerView: "auto",
+        //         spaceBetween: 16,
+        //     },
+
+        //     480: {
+        //         slidesPerView: "auto",
+        //         spaceBetween: 16,
+        //     }
+        // }
     });
 }
 
